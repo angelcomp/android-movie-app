5212e1d18a5570882020c8618fc9a173dc5c5b0c
Angelica dos Santos <santosangelicassp@gmail.com>
2021-06-01 09:51:36 -0300

    adicionando bibliotecas, binding e navigation

---
+++ app/src/main/java/com/angelica/myfavs/ui/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.angelica.myfavs.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.angelica.myfavs.R
+import com.angelica.myfavs.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
